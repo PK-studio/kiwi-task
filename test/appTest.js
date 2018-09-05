68b3d1b07fd5cc8776ebfe4748f858e1aa830ecb
Patryk Karwowski <vb.karwowski@gmail.com>
2018-09-05 21:11:43 +0100

    file setup

---
+++ test/appTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const appLocation = require("../app");
+
+describe("Test for this app: replace number with words", () => {
+  let app = null;
+  app = new appLocation.app();
+
+  describe("Initialization", () => {
+    it("It should return object", () => {
+      expect(app).to.be.a('object');
+    });
+  })
+})

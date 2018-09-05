fe9e7a3c4c5f657dce9d8c6f9203733d465b068a
Patryk Karwowski <vb.karwowski@gmail.com>
2018-09-05 22:08:37 +0100

    fix

--- test/appTest.js
+++ test/appTest.js
@@ -1,29 +1,36 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const appLocation = require("../app");
+const appLocation = require('../app');
 
-describe("Test for this app: replace number with words", () => {
+describe('Test for this app: replace number with words', () => {
   let app = null;
 
   beforeEach( () => {
     app = new appLocation.app();
   })
 
-  describe("Initialization", () => {
-    it("It should return object", () => {
+  describe('Initialization', () => {
+    it('It should return object', () => {
       expect(app).to.be.a('object');
     });
   });
 
-  describe("Run app", () => {
-    it("it should return a number when pass number", () => {
+  describe('passNumber method', () => {
+    it('it should return a number when pass number', () => {
       expect(app.passNumber(10)).to.be.a('number');
     });
     it('it should return null when pass NaN', () => {
-      expect(app.passNumber("string")).to.be.null;
+      expect(app.passNumber('string')).to.be.null;
     })
-    it("it should return null when pass nothing", () => {
+    it('it should return null when pass nothing', () => {
       expect(app.passNumber()).to.be.null;
+    })
+  });
+
+  describe('convertToArray method', ()=>{
+    it('it should return arrey of digits', ()=>{
+      app.passNumber(789)
+      expect(app.converToArray()).to.be.a('array');
     })
   });
 })

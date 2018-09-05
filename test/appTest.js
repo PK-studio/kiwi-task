9780e973f99e51a2ab21f79b4593e7514317d47f
Patryk Karwowski <vb.karwowski@gmail.com>
2018-09-05 21:28:39 +0100

    first primitive app test

--- test/appTest.js
+++ test/appTest.js
@@ -10,5 +10,11 @@ describe("Test for this app: replace number with words", () => {
     it("It should return object", () => {
       expect(app).to.be.a('object');
     });
-  })
+  });
+
+  describe("Run app", () => {
+    it("it should return empty array", () => {
+      expect(app().to.be.empty);
+    });
+  });
 })

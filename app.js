b88eac4ad3e0844c03efbf76abcc119c887831ad
Patryk Karwowski <vb.karwowski@gmail.com>
2018-09-05 22:22:06 +0100

    fix

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 module.exports = {
   app: function() {
     let number = null;
+    let arrayOfDigits = null;
+
     this.passNumber = (num) => {
       if(!num || typeof num != 'number') return number;
       number = num;
       return number;
+    };
+
+    this.converToArray= () => {
+      arrayOfDigits = number.toString().split("");
+      return arrayOfDigits
     };
   }
 };
--- test/appTest.js
+++ test/appTest.js
@@ -4,10 +4,11 @@ const appLocation = require('../app');
 
 describe('Test for this app: replace number with words', () => {
   let app = null;
+  app = new appLocation.app();
 
-  beforeEach( () => {
-    app = new appLocation.app();
-  })
+  // beforeEach( () => {
+  //   app = new appLocation.app();
+  // })
 
   describe('Initialization', () => {
     it('It should return object', () => {
@@ -16,20 +17,19 @@ describe('Test for this app: replace number with words', () => {
   });
 
   describe('passNumber method', () => {
-    it('it should return a number when pass number', () => {
-      expect(app.passNumber(10)).to.be.a('number');
-    });
     it('it should return null when pass NaN', () => {
       expect(app.passNumber('string')).to.be.null;
     })
     it('it should return null when pass nothing', () => {
       expect(app.passNumber()).to.be.null;
     })
+    it('it should return a number when pass number', () => {
+      expect(app.passNumber(10)).to.be.a('number');
+    });
   });
 
   describe('convertToArray method', ()=>{
     it('it should return arrey of digits', ()=>{
-      app.passNumber(789)
       expect(app.converToArray()).to.be.a('array');
     })
   });
